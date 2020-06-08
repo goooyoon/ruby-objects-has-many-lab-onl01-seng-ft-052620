01815142390c0dc9b71e0ca5adb844ef6236863d
Grace Kim <goooyoon@gmail.com>
2020-06-08 20:20:13 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -10,11 +10,15 @@ class Author
     post.all.select {|post| post.author == self}
   end
   
-  def add_post
+  def add_post(post)
+    post.author = self
   end
   
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
 end
-
+end
 
  def add_song_by_name(name)
     song = Song.new(name)
